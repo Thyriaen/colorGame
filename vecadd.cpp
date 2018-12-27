ee90586d8adbf0ddf732644dde2682b721429757
thyriaen <thyriaen@googlemail.com>
2018-12-27 01:58:13 +0100

    guter Mann

--- vecadd.cpp
+++ vecadd.cpp
@@ -1,98 +1,77 @@
 #include <iostream>
-#include <algorithm>
-#include <iterator>
-#ifdef __APPLE__
-#include <OpenCL/cl.hpp>
-#else
 #include <CL/cl.hpp>
-#endif
 
-using namespace std;
-using namespace cl;
-
-
-int factorial(int n) {
-    return (n <= 1) ? 1 : n * factorial(n-1);
-}
-
-
-Platform getPlatform() {
-    /* Returns the first platform found. */
-    std::vector<Platform> all_platforms;
-    Platform::get(&all_platforms);
-
-    if (all_platforms.size()==0) {
-        cout << "No platforms found. Check OpenCL installation!\n";
+int main(){
+    //get all platforms (drivers)
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(all_platforms.size()==0){
+        std::cout<<" No platforms found. Check OpenCL installation!\n";
         exit(1);
     }
-    return all_platforms[0];
-}
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
 
-
-Device getDevice(Platform platform, int i, bool display=false) {
-    /* Returns the deviced specified by the index i on platform.
-     * If display is true, then all of the platforms are listed.
-     */
-    std::vector<Device> all_devices;
-    platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+    //get default device of the default platform
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
     if(all_devices.size()==0){
-        cout << "No devices found. Check OpenCL installation!\n";
+        std::cout<<" No devices found. Check OpenCL installation!\n";
         exit(1);
     }
-
-    if (display) {
-        for (int j=0; j<all_devices.size(); j++)
-            printf("Device %d: %s\n", j, all_devices[j].getInfo<CL_DEVICE_NAME>().c_str());
-    }
-    return all_devices[i];
-}
+    cl::Device default_device=all_devices[0];
+    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
 
 
-int main() {
-    const int n = 1024;    // size of vectors
-    const int c_max = 5;   // max value to iterate to
-    const int coeff = factorial(c_max);
+    cl::Context context({default_device});
 
-    int A[n], B[n], C[n];     // A is initial, B is result, C is expected result
-    for (int i=0; i<n; i++) {
-        A[i] = i;
-        C[i] = coeff * i;
-    }
-    Platform default_platform = getPlatform();
-    Device default_device     = getDevice(default_platform, 1);
-    Context context({default_device});
-    Program::Sources sources;
+    cl::Program::Sources sources;
 
+    // kernel calculates for each element C=A+B
     std::string kernel_code=
-            "void kernel multiply_by(global int* A, const int c) {"
-            "   A[get_global_id(0)] = c * A[get_global_id(0)];"
-            "}";
-    sources.push_back({kernel_code.c_str(), kernel_code.length()});
+            "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
+            "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
+            "   }                                                                               ";
+    sources.push_back({kernel_code.c_str(),kernel_code.length()});
 
-    Program program(context, sources);
-    if (program.build({default_device}) != CL_SUCCESS) {
-        cout << "Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << std::endl;
+    cl::Program program(context,sources);
+    if(program.build({default_device})!=CL_SUCCESS){
+        std::cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<<"\n";
         exit(1);
     }
 
-    Buffer buffer_A(context, CL_MEM_READ_WRITE, sizeof(int) * n);
-    CommandQueue queue(context, default_device);
-    queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, sizeof(int)*n, A);
 
-    Kernel multiply_by = Kernel(program, "multiply_by");
-    multiply_by.setArg(0, buffer_A);
+    // create buffers on the device
+    cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*10);
+    cl::Buffer buffer_B(context,CL_MEM_READ_WRITE,sizeof(int)*10);
+    cl::Buffer buffer_C(context,CL_MEM_READ_WRITE,sizeof(int)*10);
 
-    for (int c=2; c<=c_max; c++) {
-        multiply_by.setArg(1, c);
-        queue.enqueueNDRangeKernel(multiply_by, NullRange, NDRange(n), NDRange(32));
+    int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int B[] = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
+
+    //create queue to which we will push commands for the device.
+    cl::CommandQueue queue(context,default_device);
+
+    //write arrays A and B to the device
+    queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*10,A);
+    queue.enqueueWriteBuffer(buffer_B,CL_TRUE,0,sizeof(int)*10,B);
+
+    //alternative way to run the kernel
+    cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
+    kernel_add.setArg(0,buffer_A);
+    kernel_add.setArg(1,buffer_B);
+    kernel_add.setArg(2,buffer_C);
+    queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange);
+    queue.finish();
+
+    int C[10];
+    //read result C from the device to array C
+    queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
+
+    std::cout<<" result: \n";
+    for(int i=0;i<10;i++){
+        std::cout<<C[i]<<" ";
     }
-
-    queue.enqueueReadBuffer(buffer_A, CL_TRUE, 0, sizeof(int)*n, B);
-
-    if (std::equal(std::begin(B), std::end(B), std::begin(C)))
-        cout << "Arrays are equal!" << endl;
-    else
-        cout << "Uh-oh, the arrays aren't equal!" << endl;
 
     return 0;
 }
