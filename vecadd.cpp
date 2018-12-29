67b9cc918c562e39f775792ff9d902c65eee8504
thyriaen <thyriaen@googlemail.com>
2018-12-29 04:39:36 +0100

    blub

--- vecadd.cpp
+++ vecadd.cpp
@@ -6,21 +6,21 @@ int main(){
     std::vector<cl::Platform> all_platforms;
     cl::Platform::get(&all_platforms);
     if(all_platforms.size()==0){
-        std::cout<<" No platforms found. Check OpenCL installation!\n";
+        std::cout<<" No platforms found. Check OpenCL installation!" << std::endl;
         exit(1);
     }
     cl::Platform default_platform=all_platforms[0];
-    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>() << std::endl;;
 
     //get default device of the default platform
     std::vector<cl::Device> all_devices;
     default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
     if(all_devices.size()==0){
-        std::cout<<" No devices found. Check OpenCL installation!\n";
+        std::cout<<" No devices found. Check OpenCL installation!" << std::endl;
         exit(1);
     }
     cl::Device default_device=all_devices[0];
-    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
+    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<< std::endl;
 
 
     cl::Context context({default_device});
@@ -36,7 +36,7 @@ int main(){
 
     cl::Program program(context,sources);
     if(program.build({default_device})!=CL_SUCCESS){
-        std::cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<<"\n";
+        std::cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<< std::endl;
         exit(1);
     }
 
@@ -68,7 +68,7 @@ int main(){
     //read result C from the device to array C
     queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
 
-    std::cout<<" result: \n";
+    std::cout<<" result: " << std::endl;
     for(int i=0;i<10;i++){
         std::cout<<C[i]<<" ";
     }
