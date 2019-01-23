3f57e7069b6e9c3d73b4f06ae17b4141ee60ab61
thyriaen <thyriaen@googlemail.com>
2019-01-23 12:25:23 +0100

    reworking

--- vecadd.cpp
+++ vecadd.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 #include <CL/cl.hpp>
 
-int main(){
-    //get all platforms (drivers)
-    std::vector<cl::Platform> all_platforms;
-    cl::Platform::get(&all_platforms);
-    if(all_platforms.size()==0){
-        std::cout<<" No platforms found. Check OpenCL installation!" << std::endl;
+cl::Platform platform;
+cl::Device device;
+
+void initOpenCL() {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if(platforms.empty()) {
+        std::cout << "No platforms found!" << std::endl;
         exit(1);
     }
-    cl::Platform default_platform=all_platforms[0];
-    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>() << std::endl;;
+    platform = platforms[0];
+    std::cout << "Using platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
 
-    //get default device of the default platform
-    std::vector<cl::Device> all_devices;
-    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-    if(all_devices.size()==0){
-        std::cout<<" No devices found. Check OpenCL installation!" << std::endl;
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    if(devices.empty()) {
+        std::cout << "No devices found!" << std::endl;
         exit(1);
     }
-    cl::Device default_device=all_devices[0];
-    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<< std::endl;
+    device = devices[0];
+    std::cout<< "Using device: "<< device.getInfo<CL_DEVICE_NAME>() << std::endl;
+}
 
 
-    cl::Context context({default_device});
+
+int main() {
+    initOpenCL();
+
+    cl::Context context({device});
 
     cl::Program::Sources sources;
-
-    // kernel calculates for each element C=A+B
     std::string kernel_code=
             "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
             "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
             "   }                                                                               ";
     sources.push_back({kernel_code.c_str(),kernel_code.length()});
-
     cl::Program program(context,sources);
-    if(program.build({default_device})!=CL_SUCCESS){
-        std::cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<< std::endl;
+
+    if(program.build({device}) != CL_SUCCESS){
+        std::cout <<" Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
         exit(1);
     }
-
 
     // create buffers on the device
     cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*10);
@@ -50,18 +53,18 @@ int main(){
     int B[] = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
 
     //create queue to which we will push commands for the device.
-    cl::CommandQueue queue(context,default_device);
+    cl::CommandQueue queue(context,device);
 
     //write arrays A and B to the device
     queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*10,A);
     queue.enqueueWriteBuffer(buffer_B,CL_TRUE,0,sizeof(int)*10,B);
 
     //alternative way to run the kernel
-    cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
+    cl::Kernel kernel_add = cl::Kernel(program,"simple_add");
     kernel_add.setArg(0,buffer_A);
     kernel_add.setArg(1,buffer_B);
     kernel_add.setArg(2,buffer_C);
-    queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange);
+    queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(10), cl::NullRange);
     queue.finish();
 
     int C[10];
@@ -70,8 +73,7 @@ int main(){
 
     std::cout<<" result: " << std::endl;
     for(int i=0;i<10;i++){
-        std::cout<<C[i]<<" ";
+        std::cout << C[i] << " ";
     }
-
     return 0;
 }
